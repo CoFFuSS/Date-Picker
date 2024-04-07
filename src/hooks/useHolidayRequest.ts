8e4d2f23384e4915d8cc8ff4f572ccf8df83228f
Danila <danikk066@gmail.com>
2024-04-07 13:50:42 +0300

    fix: useHolidayRequest hooks fixed and upgraded

--- src/hooks/useHolidayRequest.ts
+++ src/hooks/useHolidayRequest.ts
@@ -1,11 +1,10 @@
 import { useEffect, useState } from 'react';
 
 import { getHolidays } from '@/api/getHolidays';
-import { splitDate } from '@/utils/splitDate';
-import { getFormattedDate } from '@/utils/getFormattedDate';
+import { getFormattedDate, splitDashDate } from '@/utils/getFormattedDate';
 import { Holiday } from '@/types/interfaces';
 
-const CACHE_EXPIRATION_TIME = 24 * 60 * 60 * 1000;
+const CACHE_EXPIRATION_TIME = 7 * 24 * 60 * 60 * 1000; // 7 days in milliseconds
 
 export const useHolidayRequest = (
   year: number,
@@ -16,51 +15,48 @@ export const useHolidayRequest = (
 
   useEffect(() => {
     const fetchData = async () => {
-      try {
-        const cachedHolidays = localStorage.getItem(`holidays_${year}`);
-        const cachedDate = localStorage.getItem(`holidays_${year}_timestamp`);
-        const currentTime = new Date().getTime();
+      const cachedResponse = localStorage.getItem(`holidayData_${year}`);
+      const cachedTimestamp = localStorage.getItem(`holidayData_${year}_timestamp`);
+      const currentTime = new Date().getTime();
 
-        if (cachedHolidays && cachedDate) {
-          const parsedHolidays: Holiday[] = JSON.parse(cachedHolidays);
-          const cachedTime = parseInt(cachedDate, 10);
+      if (cachedResponse && cachedTimestamp) {
+        const parsedResponse: string[] = JSON.parse(cachedResponse);
+        const cachedTime = parseInt(cachedTimestamp, 10);
 
-          if (currentTime - cachedTime < CACHE_EXPIRATION_TIME) {
-            const formattedHolidays: string[] = parsedHolidays.map((holiday) => {
-              const { day, month, year: resYear } = splitDate(holiday.date);
-
-              return getFormattedDate(day, month, resYear);
-            });
-
-            setResponse(formattedHolidays);
+        if (currentTime - cachedTime < CACHE_EXPIRATION_TIME) {
+          if (!showHolidays) {
+            setResponse([]);
 
             return;
           }
+
+          setResponse(parsedResponse);
+
+          return;
         }
+      }
 
-        const fetchResponse: Holiday[] = await getHolidays(year);
+      const fetchResponse: Holiday[] = await getHolidays(year);
+      const formattedResponse: string[] = fetchResponse.map((holiday) => {
+        const { day, month, year: resYear } = splitDashDate(holiday.date);
 
-        const formattedResponse: string[] = fetchResponse.map((holiday) => {
-          const { day, month, year: resYear } = splitDate(holiday.date);
+        return getFormattedDate(day, month, resYear);
+      });
 
-          return getFormattedDate(day, month, resYear);
-        });
-
+      if (!showHolidays) {
+        setResponse([]);
+      } else {
         setResponse(formattedResponse);
 
-        localStorage.setItem(`holidays_${year}`, JSON.stringify(formattedResponse));
-        localStorage.setItem(`holidays_${year}_timestamp`, String(currentTime));
-
-        setApiError(null);
-      } catch (error) {
-        setApiError(error);
+        localStorage.setItem(`holidayData_${year}`, JSON.stringify(formattedResponse));
+        localStorage.setItem(`holidayData_${year}_timestamp`, String(currentTime));
       }
+
+      setApiError(null);
     };
 
-    if (showHolidays) {
-      fetchData();
-    }
-  }, [showHolidays, year]);
+    fetchData();
+  }, [year, showHolidays]);
 
   return [response, apiError];
 };
--- src/utils/getFormattedDate.ts
+++ src/utils/getFormattedDate.ts
@@ -1,6 +1,29 @@
+import { splitDate } from './splitDate';
+
 export const getFormattedDate = (day: number, month: number, year: number) => {
   const paddedDay = day < 10 ? `0${day}` : day.toString();
   const paddedMonth = month < 10 ? `0${month}` : month.toString();
 
   return `${paddedDay}.${paddedMonth}.${year}`;
+};
+
+export const splitAndFormat = (value: string) => {
+  const { day, month, year } = splitDate(value);
+
+  return getFormattedDate(day, month, year);
+};
+
+export const splitDashDate = (dateString: string): { day: number; month: number; year: number } => {
+  const parts = dateString.split('-');
+
+  if (parts.length === 3) {
+    const [yearStr, monthStr, dayStr] = parts;
+    const year = parseInt(yearStr, 10);
+    const month = parseInt(monthStr, 10);
+    const day = parseInt(dayStr, 10);
+
+    return { day, month, year };
+  }
+
+  throw new Error('Invalid date format');
 };
